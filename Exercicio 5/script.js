8e66d0d262863b8c2e3e0665aceeb30c16b7cd0e
Arturfgamer2002 <130005874+Arturfgamer2002@users.noreply.github.com>
2023-10-23 23:02:41 -0300

    Add files via upload

--- Exercicio 5/script.js
+++ Exercicio 5/script.js
@@ -2,6 +2,9 @@ const modeToggle = document.getElementById('mode-toggle');
 const body = document.body;
 const lightIcon = document.getElementById('light-icon');
 const darkIcon = document.getElementById('dark-icon');
+const menuResponsivo = () => {
+  document.querySelector(".topbar").classList.toggle("responsive");
+};
 
 modeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
