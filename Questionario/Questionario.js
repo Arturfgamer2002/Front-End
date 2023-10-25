cbbe3b147572f77ed8f1d602217e647f7adf8daa
Arturfgamer2002 <130005874+Arturfgamer2002@users.noreply.github.com>
2023-10-24 23:14:29 -0300

    Add files via upload

---
+++ Questionario/Questionario.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    question:
+      "Qual é a função usada para selecionar um elemento pelo seu id no JavaScript?",
+    options: ["getElementById", "selectElement", "selectById", "queryElement"],
+    answer: 0,
+  },
+  {
+    question: "Qual propriedade CSS é usada para definir a cor de fundo de um elemento?",
+    options: ["color", "background-color", "bg-color", "fill-color"],
+    answer: 1,
+  },
+  {
+    question:
+      "Qual é o elemento HTML usado para criar um link em uma página da web?",
+    options: ["&lt;link&gt;", "&lt;a&gt;", "&lt;href&gt;", "&lt;connect&gt;"],
+    answer: 1,
+  },
+  {
+  question:
+      "Qual é a tag usada para criar uma lista não ordenada em HTML?",
+    options: ["&lt;ol&gt;", "&lt;list&gt;", "&lt;ul&gt;", "&lt;unordered&gt;"],
+    answer: 2,
+  },
+  {
+    question: "Qual propriedade CSS é usada para alterar a fonte do texto?",
+    options: ["font-family", "text-font", "font-style", "text-type"],
+    answer: 0,
+  },
+  {
+    question:
+      "Qual é a forma correta de declarar uma variável em JavaScript?",
+    options: ["variable = myVariable;", "var myVariable;", "v myVariable;", "let myVariable;"],
+    answer: 1,
+  },
+  {
+    question: "Qual é a propriedade usada para ajustar o espaçamento entre linhas em CSS?",
+    options: ["line-height", "space-between", "line-spacing", "spacing"],
+    answer: 0,
+  },
+  {
+    question: "Qual elemento HTML é usado para criar um botão em uma página da web?",
+    options: ["&lt;button&gt;", "&lt;submit&gt;", "&lt;click&gt;", "&lt;input&gt;"],
+    answer: 0,
+  },
+  {
+    question: "O que CSS representa?",
+    options: ["Colorful Style Sheets", "Creative Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+    answer: 2,
+  },
+  {
+    question:
+      "Qual é a forma correta de vincular um arquivo CSS externo a uma página HTML?",
+    options: ["&lt;link rel='stylesheet' type='text/css' href='styles.css'&gt;", "&lt;style src='styles.css'&gt;&lt;/style&gt;", "&lt;style link='styles.css'&gt;&lt;/style&gt;", "&lt;stylesheet&gt;styles.css&lt;/stylesheet&gt;"],
+    answer: 0,
+  },
+];
